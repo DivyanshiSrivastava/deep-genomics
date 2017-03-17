f5672cbe86bdb18adbce0af0d87691d5b9e7453b
DivyanshiSrivastava <s.divyanshi91@gmail.com>
2017-03-17 15:54:33 -0400

    Adding convolution NN code

---
+++ tensorflow/convolution_NNs/binary/scanwg.py
@@ -0,0 +1,115 @@
+# Train a single layer neural network ( CNN ) using sequence.
+# Impementation: tensorflow v. 0.11
+
+import sys
+import tensorflow as tf
+import numpy as np
+import read
+# from sklearn.model_selection import train_test_split
+
+def read_single_example(filename, record_defaults):
+    # construct a queue for all the files.
+    filename_queue = tf.train.string_input_producer([filename],
+                                                    num_epochs = 1,
+                                                    name = "myQueue")
+    # create a reader. 
+    reader = tf.TextLineReader()
+    key, value = reader.read(filename_queue)
+    int_example = tf.decode_csv(value, record_defaults = record_defaults)
+    example = tf.cast(int_example, tf.float32)
+    return example
+
+def variables(shapeW, shapeB, nameW, nameB):
+    """ Defining the initial convolution """
+    initialW = tf.truncated_normal( shapeW , stddev=0.1)
+    initialB = tf.constant(0.1, shape=shapeB)
+    return tf.Variable(initialW, name = nameW), tf.Variable(initialB, name = nameB)
+
+def conv1d(x, W):
+    return tf.nn.conv1d(x, W, stride=1, padding='SAME')
+
+def max_pool(x,n):
+    return tf.nn.max_pool(x, ksize=[1, 1, n, 1],
+                        strides=[1, 1, n, 1], padding='SAME')
+
+def nn(xd,yd,zd,bd,fc_nodes,examples_batch,n,keep_prob):
+    """ convolve over feature matrix """
+
+    W_conv1, b_conv1 = variables([20,4,32], [32], 'W_conv1', 'b_conv1')
+    x_image = tf.reshape(examples_batch, [-1,n,4])                          # [batch, in_width, in_channels]
+    h_conv1 = tf.nn.relu(conv1d(x_image, W_conv1) + b_conv1)   # [batch, out_width, out_channels] // eq // [batch, 1, out_width, out_channels]
+    x_image2 = tf.reshape(h_conv1,[-1,1,n,32])
+    h_pool1 = max_pool(x_image2,n)
+
+    # do a fully connected layer
+    W_fc1, b_fc1 = variables( [1 * 1 * 32,fc_nodes], [fc_nodes], 'W_fc1', 'b_fc1') 
+    h_pool1_flat = tf.reshape(h_pool1, [-1, 1 * 1 * 32])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
+
+    # dropout
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    # final layer
+    W_fc2, b_fc2 = variables([fc_nodes, 2], [2], 'W_fc2', 'b_fc2')
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+    return y_conv
+
+
+def run_conv(xd,yd,zd,bd,fc_nodes,dropout,n, examples_batch, keep_prob):
+    """ calculating loss & training """
+    y_conv = nn( xd, yd, zd, bd, fc_nodes,examples_batch,n,keep_prob)
+    y_conv_softmax = tf.nn.softmax(y_conv) # for auROC/auPRC calculations.
+    
+    init = tf.initialize_all_variables()
+    init_local = tf.initialize_local_variables()
+    saver = tf.train.Saver()
+
+    # running the tensorflow graph - training
+
+    with tf.Session() as sess:
+        
+        print "In session"
+        sess.run(init)
+        sess.run(init_local)
+        saver.restore(sess, tf.train.latest_checkpoint('./'))
+    
+        # start enque threads:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord = coord)
+        steps = 1
+        try:
+          while not coord.should_stop():
+            temp = y_conv_softmax.eval(feed_dict = { keep_prob: 1})
+            f = "probas_v2/outfile" + str(steps)
+            np.savetxt(f, temp)
+            print "Done step %d" % steps
+            steps = steps + 1
+        except tf.errors.OutOfRangeError:
+          print "Reached End"
+        finally:
+          coord.request_stop()
+
+        # Wait for the threads to finish
+        coord.join(threads)
+        sess.close()
+
+def main():
+    
+    n = 200
+    record_defaults = list()
+    for i in range(800):
+        record_defaults.append([0])
+
+    # get single examples
+    example = read_single_example(sys.argv[1], record_defaults)
+    # groups examples into batches randomly
+    examples_batch = tf.train.batch([example], batch_size=50000,capacity=200000, allow_smaller_final_batch=True)
+    
+    keep_prob = tf.placeholder(tf.float32)
+
+    run_conv(4,25,1,16, 512, 0.5, n, examples_batch, keep_prob)
+
+if __name__ == "__main__":
+    main()
+
---
+++ tensorflow/convolution_NNs/multilabel/cnn_1d_multilabel.py
@@ -0,0 +1,120 @@
+# Train a single layer neural network ( CNN ) using sequence.
+# Impementation: tensorflow v. 0.11
+
+import sys
+import tensorflow as tf
+import numpy as np
+import read_multilabel
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import average_precision_score
+
+def variables(shapeW, shapeB, nameW, nameB):
+    """ Defining the initial convolution """
+    initialW = tf.truncated_normal( shapeW , stddev=0.1)
+    initialB = tf.constant(0.1, shape=shapeB)
+    return tf.Variable(initialW, name = nameW), tf.Variable(initialB, name = nameB)
+
+def conv1d(x, W):
+    return tf.nn.conv1d(x, W, stride=1, padding='SAME')
+
+def max_pool(x,n):
+    return tf.nn.max_pool(x, ksize=[1, 1, n, 1],
+                        strides=[1, 1, n, 1], padding='SAME')
+
+def nn(xd,yd,zd,bd,fc_nodes,x,n,keep_prob):
+    """ convolve over feature matrix """
+
+    W_conv1, b_conv1 = variables([20,4,32], [32], 'W_conv1', 'b_conv1')
+    x_image = tf.reshape(x, [-1,n,4])                          # [batch, in_width, in_channels]
+    h_conv1 = tf.nn.relu(conv1d(x_image, W_conv1) + b_conv1)   # [batch, out_width, out_channels] // eq // [batch, 1, out_width, out_channels]
+    x_image2 = tf.reshape(h_conv1,[-1,1,n,32])
+    h_pool1 = max_pool(x_image2,n)
+
+    # do a fully connected layer
+    W_fc1, b_fc1 = variables( [1 * 1 * 32,fc_nodes], [fc_nodes], 'W_fc1', 'b_fc1') 
+    h_pool1_flat = tf.reshape(h_pool1, [-1, 1 * 1 * 32])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
+
+    # dropout
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    # final layer
+    W_fc2, b_fc2 = variables([fc_nodes, 4], [4], 'W_fc2', 'b_fc2')
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+    return y_conv
+
+
+def run_conv(xd,yd,zd,bd,fc_nodes,dropout,n,x,y_, keep_prob, X_train, X_test, y_train, y_test):
+    """ calculating loss & training """
+    y_conv = nn( xd, yd, zd, bd, fc_nodes,x,n,keep_prob)
+    y_conv_softmax = tf.nn.softmax(y_conv) # for auROC/auPRC calculations.
+    
+    # balancing the unequal classes. 
+    class_weight = tf.constant([[0.3,0.1,0.3,0.2]])
+    ce = tf.nn.sigmoid_cross_entropy_with_logits(logits = y_conv, labels = y_)
+    xent = tf.multiply(ce, class_weight, name="xent_raw") #shape [1, batch_size]
+    cross_entropy = tf.reduce_mean(xent) #shape 1 
+    
+    #cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = y_conv,labels = y_))
+    
+    train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    init = tf.initialize_all_variables()
+    saver = tf.train.Saver(write_version=tf.train.SaverDef.V1)
+    #saver = tf.train.Saver()
+
+    # running the tensorflow graph - training
+
+    with tf.Session() as sess:
+        
+        print "In session"
+        sess.run(init)
+        batch_size = 100
+    
+	epochs = 0
+
+        while epochs < 50:
+		
+	    idx = 0
+	    print len(X_train)
+	    while idx + batch_size < len(X_train):
+                batch_xs, batch_ys = X_train[idx:idx + batch_size], y_train[idx:idx+batch_size]
+	        idx = idx + batch_size
+		# train
+                sess.run(train_step, feed_dict = { x: batch_xs, y_ : batch_ys, keep_prob: dropout})
+            
+	    # test_accuracy = accuracy.eval(feed_dict={x: X_test, y_ : y_test, keep_prob: 1})
+	    # print "Epoch %d, test accuracy %f" % (epochs, test_accuracy)
+            test_probas = y_conv_softmax.eval(feed_dict= {x: X_test, y_ : y_test, keep_prob: 1})
+            
+            with open(sys.argv[2], "a") as fp:
+                read_multilabel.metrics(test_probas, y_test, 4, fp)
+            epochs += 1
+        #save_path = saver.save(sess, "trained_model.ckpt")
+
+def main():
+    
+    # reading the feature matrix & splitting into test and train. 
+
+    X = read_multilabel.get_matrix(sys.argv[1] + ".shuf.seq")
+    y = read_multilabel.get_labels(sys.argv[1] + ".shuf.conditions")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    print y_test.shape
+    print X_test.shape
+    print y_test[:10]
+    
+    n = int(sys.argv[3])
+
+    # creating placeholders for the features (x) , and the labels (y).
+
+    x  = tf.placeholder(tf.float32, shape = (None, n*4)) # One-hot, so n*4
+    y_ = tf.placeholder(tf.float32, shape = (None, 4))   # k = no_of_classes = 2
+    keep_prob = tf.placeholder(tf.float32)
+ 
+    run_conv(4,25,1,16, 512, 0.5, n, x, y_, keep_prob, X_train, X_test, y_train, y_test)
+
+if __name__ == "__main__":
+    main()
