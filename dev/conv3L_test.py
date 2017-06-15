23195357a34e0156931538791950eeb8c645ac0f
DivyanshiSrivastava <s.divyanshi91@gmail.com>
2017-06-15 16:18:20 -0400

    adding fns for testing

--- dev/conv3L_test.py
+++ dev/conv3L_test.py
@@ -25,31 +25,37 @@ def map_bases(line):
 
 # load model 
 
-model = load_model("conv3L_model")
+model = load_model("conv3L_model.h5py")
 feature_dict = { 'A' : [1, 0, 0, 0], 'T' : [0,1,0,0], 'G' : [0,0,1,0],'C' : [0,0,0, 1], 'N': [0,0,0,0]}
-n = 1000
+n = 5000
 
 # iterate over test
-probas = []
 lc = 0
-with open("/group/lab/divyanshi/data/EB/factors/Ascl1/train_test_split/seqfiles/test.dat", "r") as f:
+with open(sys.argv[1] + ".seq", "r") as f:
     while True:
         chunk = list(islice(f, n))
         if chunk != []:
             lc += 1
-            chunk = [map_bases(x) for x in next_n_lines]
+            print lc
+            chunk = [map_bases(x) for x in chunk]
             x = np.array(chunk)
             x_rs = np.reshape(x,(-1,200,4))
-            print x_rs.shape
-            probas.append(test(x_rs))
+            try:
+                hold = np.ravel(np.array(test(x_rs)))
+                probas = np.hstack((probas, hold))
+            except:
+                probas = np.ravel(np.array(test(x_rs)))
 	if not chunk:
             break
 
-probas = np.ravel(np.array(probas))
-y_test = np.loadtxt("/group/lab/divyanshi/data/EB/factors/Ascl1/train_test_split/seqfiles/test.labels")
+y_test = np.loadtxt((sys.argv[1] + ".labels"))
+threshold = lambda t: 1 if t >= 0.5 else 0 
+npthresh = np.vectorize(threshold)
+y_pred = npthresh(probas)
 
 # measure performance
 roc_auc = sklearn.metrics.roc_auc_score(y_test, probas)
 prc = sklearn.metrics.average_precision_score(y_test, probas)
 print "test roc", roc_auc
 print "test prc", prc
+print sklearn.metrics.confusion_matrix(y_test, y_pred)
